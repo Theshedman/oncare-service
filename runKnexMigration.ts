4ded843e4dc681305d9ccbbb5af0dfaaad180cb1
Shedrack Ajaegbu <shedrack3@gmail.com>
2022-02-09 20:23:04 +0100

    log standard output

--- runKnexMigration.ts
+++ runKnexMigration.ts
@@ -2,6 +2,7 @@ import shell from 'shelljs';
 
 export class Migration {
   public static run() {
-    shell.exec('yarn migrate:up', { silent: true });
+    // shell.exec('yarn migrate:up', { silent: true });
+    shell.exec('yarn migrate:up');
   }
 }
